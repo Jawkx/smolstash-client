cba5a37f856c514d12487728bac9617e88208aa6
jaw <jawkaixuan99@gmail.com>
2024-11-29 17:04:39 +0800

    add react compiler cause why not

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -11,7 +12,11 @@ export default [
 	{
 		rules: {
 			"@typescript-eslint/no-unused-vars": "warn",
+			"@typescript-eslint/no-require-imports": "off",
+			"react-compiler/react-compiler": "error",
+		},
+		plugins: {
+			"react-compiler": reactCompiler,
 		},
 	},
 ];
-
