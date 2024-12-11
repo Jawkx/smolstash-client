b571144503fa614fe4ffd6ef5ae8d5c17aad8792
jaw <jawkaixuan99@gmail.com>
2024-12-12 01:49:47 +0800

    extend config to whole components directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	// NOTE: Update this to include the paths to all of your component files.
 	content: [
 		"./app/**/*.{js,jsx,ts,tsx}",
-		"./@/components/ui/**/*.{js,jsx,ts,tsx}",
+		"./@/components/**/*.{js,jsx,ts,tsx}",
 	],
 	presets: [require("nativewind/preset")],
 	theme: {
