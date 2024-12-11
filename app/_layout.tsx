0126c844ddb4cb6d275bf09c7b9b4ae22957fdd4
jaw <jawkaixuan99@gmail.com>
2024-12-11 22:09:55 +0800

    flex correctly

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout() {
 							<Stack.Screen
 								name="login"
 								options={{
-									presentation: "transparentModal",
+									presentation: "containedTransparentModal",
 									animation: "fade",
 								}}
 							/>
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 import { Button, LinkButton } from "@ui/button";
 import { Text } from "@ui/text";
 import { Moon, Sun } from "@lib/icons";
@@ -11,11 +11,13 @@ const IndexPage = () => {
 	return (
 		<View className="bg-background w-full h-full">
 			<View className="mt-safe flex-row justify-center items-center h-screen w-screen bg-background">
-				<View className="absolute top-6 right-6 flex-row">
+				<View
+					className={`absolute top-6 flex-row w-full px-6 ${Platform.OS === "web" ? "justify-end gap-2" : "justify-between"}`}
+				>
 					<LinkButton href="/login">
 						<Text className="font-semibold">Sign Up / Login</Text>
 					</LinkButton>
-					<View className="w-0.5" />
+
 					<Button variant="link" onPress={toggleColorScheme}>
 						{colorScheme === "dark" ? (
 							<Moon className="text-foreground" />
