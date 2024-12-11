60e38f492e63a8e55800d00962ec990210d199ef
jaw <jawkaixuan99@gmail.com>
2024-12-11 21:32:24 +0800

    rework dark mode toggle for web

--- @/lib/useColorScheme.tsx
+++ @/lib/useColorScheme.tsx
@@ -1,12 +1,30 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useColorScheme as useNativewindColorScheme } from "nativewind";
 
 export function useColorScheme() {
 	const { colorScheme, setColorScheme, toggleColorScheme } =
 		useNativewindColorScheme();
+
+	const toggleColorSchemeInAsyncStorage = () => {
+		const isLightScheme = colorScheme === "light";
+		if (isLightScheme) {
+			AsyncStorage.setItem("theme", "dark");
+		} else {
+			AsyncStorage.setItem("theme", "light");
+		}
+
+		document.documentElement.classList.replace(
+			isLightScheme ? "root" : "dark-root",
+			isLightScheme ? "dark-root" : "root",
+		);
+
+		toggleColorScheme();
+	};
+
 	return {
 		colorScheme: colorScheme ?? "dark",
 		isDarkColorScheme: colorScheme === "dark",
 		setColorScheme,
-		toggleColorScheme,
+		toggleColorScheme: toggleColorSchemeInAsyncStorage,
 	};
 }
