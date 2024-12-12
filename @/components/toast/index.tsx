c86f85c7c513dbbf982bc411211ff55cf09f038e
jaw <jawkaixuan99@gmail.com>
2024-12-12 15:40:03 +0800

    Add toast

--- @/components/toast/index.tsx
+++ @/components/toast/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import {
-	BaseToast,
-	BaseToastProps,
-	ToastConfig,
-} from "react-native-toast-message";
+import { BaseToastProps, ToastConfig } from "react-native-toast-message";
 import { View, Platform } from "react-native";
 import { Text } from "../ui/text";
+import { CircleCheck, CircleX } from "@/lib/icons";
 
 export const toastConfig: ToastConfig = {
 	success: ({ text1 }: BaseToastProps) => {
@@ -13,11 +10,26 @@ export const toastConfig: ToastConfig = {
 			<View
 				className={`w-full h-12 flex-row ${Platform.OS === "web" ? "justify-end px-8" : "justify-center"}`}
 			>
-				<View className="h-full w-[90%] max-w-[400] bg-card rounded-lg border-emerald-600 border-2 flex-row items-center">
+				<View className="h-full w-[90%] max-w-[400] flex-row bg-card border-green-600 border-2 rounded-lg items-center">
 					<View className="w-3" />
-					<View className="w-2 h-[60%] rounded-lg bg-emerald-600" />
+					<CircleCheck className="rounded-lg text-green-600" />
 					<View className="w-1.5" />
-					<Text>{text1}</Text>
+					<Text className="text-green-600">{text1}</Text>
+				</View>
+			</View>
+		);
+	},
+
+	error: ({ text1 }: BaseToastProps) => {
+		return (
+			<View
+				className={`w-full h-12 flex-row ${Platform.OS === "web" ? "justify-end px-8" : "justify-center"}`}
+			>
+				<View className="h-full w-[90%] max-w-[400] flex-row bg-card border-red-600 border-2 rounded-lg items-center">
+					<View className="w-3" />
+					<CircleX className="rounded-lg text-red-600" />
+					<View className="w-1.5" />
+					<Text className="text-red-600">{text1}</Text>
 				</View>
 			</View>
 		);
--- @/lib/icons/index.ts
+++ @/lib/icons/index.ts
@@ -1,7 +1,9 @@
-import { Moon, Sun } from "lucide-react-native";
+import { Moon, Sun, CircleCheck, CircleX } from "lucide-react-native";
 import { iconWithClassName } from "./iconWithClassName";
 
 iconWithClassName(Moon);
 iconWithClassName(Sun);
+iconWithClassName(CircleCheck);
+iconWithClassName(CircleX);
 
-export { Moon, Sun };
+export { Moon, Sun, CircleCheck, CircleX };
