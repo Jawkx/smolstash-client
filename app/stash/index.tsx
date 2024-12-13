8aa35fb41461dd3c344febf2bc456ed8bd099752
jaw <jawkaixuan99@gmail.com>
2024-12-13 20:02:32 +0800

    make index screen looks nicer

--- app/stash/index.tsx
+++ app/stash/index.tsx
@@ -3,11 +3,17 @@ import { Text } from "@/components/ui/text";
 import { useColorScheme } from "@/lib/useColorScheme";
 import { useAuth } from "@clerk/clerk-expo";
 import { useRouter } from "expo-router";
+import { Moon, Sun } from "lucide-react-native";
 import React from "react";
-import { View } from "react-native";
+import { Platform, TouchableOpacity, View } from "react-native";
+import * as Clipboard from "expo-clipboard";
+import Toast from "react-native-toast-message";
 
 const Stash = () => {
-	const { userId, signOut } = useAuth();
+	const { userId, signOut, getToken } = useAuth();
+	const { toggleColorScheme, colorScheme } = useColorScheme();
+	const [accessToken, setAccessToken] = React.useState<string | null>(null);
+
 	const router = useRouter();
 
 	const handleSignOut = () => {
@@ -15,12 +21,47 @@ const Stash = () => {
 		signOut();
 	};
 
+	React.useEffect(() => {
+		getToken().then((token) => setAccessToken(token));
+	}, []);
+
+	const handleCopyToken = () => {
+		if (accessToken) {
+			Clipboard.setStringAsync(accessToken).then(() => {
+				Toast.show({ type: "success", text1: "access token copied" });
+			});
+		}
+	};
+
 	return (
-		<View className="w-full h-full">
-			<Text>Work In Progress</Text>
-			<Button onPress={handleSignOut}>
-				<Text>Log out</Text>
-			</Button>
+		<View className="w-full h-full justify-center items-center">
+			<View className="absolute top-6 flex-row w-full px-6 justify-end">
+				<Button onPress={handleSignOut}>
+					<Text className="font-semibold">Sign out</Text>
+				</Button>
+
+				<Button variant="link" onPress={toggleColorScheme}>
+					{colorScheme === "dark" ? (
+						<Moon className="text-foreground" />
+					) : (
+						<Sun className="text-foreground" />
+					)}
+				</Button>
+			</View>
+
+			<View className="px-12 max-w-[500]">
+				<Text className="text-center">Work In Progress</Text>
+				<View className="h-8" />
+				<TouchableOpacity onPress={handleCopyToken}>
+					<Text
+						className="text-center"
+						numberOfLines={1}
+						ellipsizeMode="middle"
+					>
+						{accessToken}
+					</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 };
