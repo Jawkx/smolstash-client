d6d0ca7e78babe5698a772d4aa06c07fee6690bd
jaw <jawkaixuan99@gmail.com>
2024-12-31 10:55:58 +0800

    remove unused import

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import { Platform, View } from "react-native";
-import { Button, LinkButton } from "@ui/button";
-import { Text } from "@ui/text";
-import { Moon, Sun } from "@lib/icons";
 import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 
