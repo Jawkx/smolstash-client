0f0c2956b468500552b989d831880e9badb62d19
jaw <jawkaixuan99@gmail.com>
2024-12-31 10:55:51 +0800

    create one unify stashScreen/index

--- @/components/screens/StashScreen/index.tsx
+++ @/components/screens/StashScreen/index.tsx
@@ -1,11 +1,24 @@
+import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { useNavigation } from "expo-router";
 import React from "react";
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 
 export const StashScreen = () => {
+	const navigation = useNavigation() as DrawerNavigationProp<any>;
 	return (
-		<View>
-			<Text> Select a stash </Text>
+		<View className="items-center justify-center flex-1">
+			<Text className="font-bold text-xl">Select a stash to continue</Text>
+			{Platform.OS !== "web" ? (
+				<Button
+					onPress={() => {
+						navigation.openDrawer();
+					}}
+				>
+					<Text>Select a stash</Text>
+				</Button>
+			) : null}
 		</View>
 	);
 };
